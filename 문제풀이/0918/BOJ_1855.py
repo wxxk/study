8abb528a09e884ca036f75b51a1535d4949e4a6b
wxxk <dwde22@gmail.com>
2022-09-18 21:54:52 +0900

    스터디

--- 문제풀이/0918/BOJ_1855.py
+++ 문제풀이/0918/BOJ_1855.py
@@ -1,4 +1,22 @@
+from audioop import reverse
 import sys
 sys.stdin = open('1855.txt')
 
-n = int(input())
+k = int(input())
+word = input()
+arr = []
+
+for i in range(0, len(word), 3):
+    arr.append(list(word[i:i+k]))
+
+for j in range(len(arr)):
+    if j%2 != 0:
+        arr[j].reverse()
+
+answer = list(map(list, zip(*arr)))
+
+for k in answer:
+    for l in k:
+        print(l, end='')
+
+
