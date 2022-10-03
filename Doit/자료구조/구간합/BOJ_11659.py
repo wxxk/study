b1e9d653e334295b59d3bbbb7a22d0d03b098873
wxxk <dwde22@gmail.com>
2022-10-03 23:53:15 +0900

    부분합 수정

--- Doit/자료구조/구간합/BOJ_11659.py
+++ Doit/자료구조/구간합/BOJ_11659.py
@@ -2,8 +2,8 @@ import sys
 
 sys.stdin = open("input.txt")
 
-N, M = map(int, input().split())
-num = list(map(int, input().split()))
+N, M = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
 
 sum_num = [0]  # 합 배열(input의 index범위가 1~5이기때문에 0을 추가해서 인덱스 범위를 맞춰준다!)
 temp = 0  # 합을 계산하기 위한 임시 변수
@@ -15,6 +15,6 @@ for i in num:
 
 # 부분합 구하기
 for j in range(M):
-    x, y = map(int, input().split())
+    x, y = map(int, sys.stdin.readline().split())
 
     print(sum_num[y] - sum_num[x - 1])
